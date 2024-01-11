346943a27084d08e6fc2444aabbfe07e194adb3a
CarolMontteiro <carolmonteiro1206@gmail.com>
2024-01-11 20:31:17 -0300

    criando estrutura básica

---
+++ resources/views/template.blade.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <script defer src="/resources/js/jsapp.js"></script>
+    <script src="https://kit.fontawesome.com/8005c041ac.js" crossorigin="anonymous"></script>
+    <title>CodeZeus</title>
+</head>
+<body>
+<header class="bg-stone-900">
+    <div class="container">
+        <h1 class="text-white text-2xl py-6">CodeZeus</h1>
+    </div>
+</header>
+
+<section id="introduction" class="bg-white min-h-[90vh] grid place-items-center items-center">
+    <div class="container">
+        <div class="flex">
+            <div class="w-1/2">
+                <h3 class="">Olá, eu sou o</h3>
+                <h1 class="">Alexandre Schielke</h1>
+                <h3 class="">Desenvolveror Full-Stack</h3>
+                <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            </div>
+            <div class="w-1/2 h-auto">
+                <img class="h-300 w-300" src="/image/fotodark.jpeg" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="about" class="bg-stone-900 min-h-screen grid place-items-center items-center">
+    <div class="container">
+        <div class="bg-stone-800 border-2 border-white/10 rounded-lg p-8">
+            <div class="pb-10">
+                <h4 class="text-lg text-white pb-5">Quem sou</h4>
+                <h2 class="text-2xl text-white font-semibold pb-4">Alexandre Schielke</h2>
+                <p class="text-white">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque facilisis diam et condimentum sagittis.
+                    Etiam sollicitudin, magna a pretium consectetur, arcu augue imperdiet quam, vel consectetur massa enim nec
+                    dolor. Nullam libero mauris, convallis vitae sapien vel, malesuada tempor libero.
+                </p>
+            </div>
+
+            <div class="social-media flex">
+                <a href="#" class="border-2 border-white/10 p-5 mr-3"><i class="fa-brands fa-github text-2xl text-white font-semibold"></i></a>
+                <a href="#" class="border-2 border-white/10 p-5 mr-3"><i class="fa-brands fa-linkedin-in text-2xl text-white font-semibold"></i></a>
+                <a href="#" class="border-2 border-white/10 p-5"><i class="fa-regular fa-envelope text-2xl text-white font-semibold"></i></a>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section id="skill" class="min-h-screen grid place-items-center items-center">
+    <div class="container">
+        <h2 class="text-3xl font-semibold pb-16">Conhecimentos</h2>
+
+        <div class="cards-skill flex flex-wrap gap-6">
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+            <div class="bg-amber-200 w-28 flex flex-col justify-center items-center aspect-square rounded-tl-3xl">
+                <p class="text-md pb-2">Lorem ipsum</p>
+                <i class="fa-brands fa-html5 text-5xl"></i>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section id="projects" class="bg-stone-900 min-h-screen grid place-items-center items-center">
+    <div class="container">
+        <h2 class="text-white text-3xl font-semibold pb-16">Conhecimentos</h2>
+
+        <div class="cards-projects">
+
+        </div>
+    </div>
+</section>
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,29 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        container: {
+            padding: '2rem',
+        },
+        extend: {
+            container: {
+                // default breakpoints but with 40px removed
+                screens: {
+                    sm: '640px',
+                    md: '768px',
+                    lg: '1024px',
+                    xl: '1200px',
+                    xll: '1496px',
+                },
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    sm: '2rem',
+                    lg: '4rem',
+                    xl: '5rem',
+                    '2xl': '6rem',
+                },
+            },
+        },
     },
     plugins: [],
 }
